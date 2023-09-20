3f65f224934e3d19a3e2911856ba5076da38d377
wmbolles <wassimbolles18@gmail.com>
2023-09-20 20:47:54 +0100

    check.sh

---
+++ fileDescriptor_functions/check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gcc -Wall -Wextra -Werror orignal_functions/puts.c -o outputs/puts
+gcc -Wall -Wextra -Werror libft_test/puts.c ../../ft_putstr_fd.c ../../ft_strlen.c -o outputs/putstr
+
+cd outputs
+./puts > puts_output.txt
+./putstr > putstr_output.txt
+
+if diff -q puts_output.txt putstr_output.txt >/dev/null; then
+    echo -e "putstr : \033[32mOK\033[0m"
+else
+    echo -e "putstr : \033[31mKO\033[0m"
+    diff puts_output.txt putstr_output.txt > diff_putstr_fd
+    echo diff_putstr_fd
+fi
+
+rm puts_output.txt putstr_output.txt
+
+cd ..
+
