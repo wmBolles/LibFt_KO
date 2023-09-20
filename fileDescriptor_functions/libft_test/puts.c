9a64d5dae572067c4befe22a24d1d9a2941d2c7a
wmbolles <wassimbolles18@gmail.com>
2023-09-20 20:47:44 +0100

    libft_test/

---
+++ fileDescriptor_functions/libft_test/puts.c
@@ -0,0 +1,11 @@
+#include "../../../libft.h"
+
+int	main(void)
+{
+	char	*str = "just a test!!!!!!???\0stop here";
+	ft_putstr_fd(str, 1);
+	ft_putstr_fd("\n", 1);
+	ft_putstr_fd("wmbolles is the best\n", 1);
+	ft_putstr_fd("%\t\n", 1);
+	return 0x0;
+}
