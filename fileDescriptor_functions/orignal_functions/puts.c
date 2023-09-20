4935974038c704bd313ffd3ec57134edc7a58139
wmbolles <wassimbolles18@gmail.com>
2023-09-20 20:48:07 +0100

    orignal_functions/

---
+++ fileDescriptor_functions/orignal_functions/puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char    *str = "just a test!!!!!!???\0stop here";
+        puts(str);
+        puts("wmbolles is the best");
+        puts("%\t");
+        return 0x0;
+}
+
