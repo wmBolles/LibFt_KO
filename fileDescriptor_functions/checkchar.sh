fdb1441908cb7e3b10cc1f67a8eb0b8e93e9d5e4
wmBolles <wassimbolles18@gmail.com>
2023-09-21 11:52:13 +0100

    putchar test Script

---
+++ fileDescriptor_functions/checkchar.sh
@@ -0,0 +1,17 @@
+gcc -Wall -Wextra -Werror orignal_functions/putchar.c -o putchar
+gcc -Wall -Wextra -Werror libft_test/putchar.c ../../ft_putchar_fd.c -o ft_putchar_fd
+
+./putchar > orputchar
+./ft_putchar_fd > fdputchar
+
+if diff -q  orputchar fdputchar >/dev/null; then
+    echo -e "putchar : \033[32mOK\033[0m"
+else
+    echo -e "putchar : \033[31mKO\033[0m"
+    diff orputchar fdputchar > diff_putchar_fd
+    cat diff_putchar_fd
+
+fi
+rm orputchar fdputchar putchar ft_putchar_fd
+
+cd ..
