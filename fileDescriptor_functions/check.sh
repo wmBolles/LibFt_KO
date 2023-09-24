4b4ed88642dc7807a0afd4407a8ef7d8bc086e2c
wmBolles <wassimbolles18@gmail.com>
2023-09-24 21:44:44 +0100

    Update And Fix bash errors

--- fileDescriptor_functions/check.sh
+++ fileDescriptor_functions/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gcc -Wall -Wextra -Werror orignal_functions/puts.c -o outputs/puts
-gcc -Wall -Wextra -Werror libft_test/puts.c ../../ft_putstr_fd.c ../../ft_strlen.c -o outputs/putstr
+gcc -Wall -Wextra -Werror libft_test/puts.c  ../../ft_*.c -o outputs/putstr
 
 cd outputs
 ./puts > puts_output.txt
@@ -11,8 +11,8 @@ if diff -q puts_output.txt putstr_output.txt >/dev/null; then
     echo -e "ft_putstr : \033[32mOK\033[0m"
 else
     echo -e "ft_putstr : \033[31mKO\033[0m"
-    diff -q puts_output.txt putstr_output.txt > diff
-    cat diff
+    diff -q puts_output.txt putstr_output.txt > ../traces/dputs.diff
+    cat ../traces/puts.diff
 fi
 
 rm puts_output.txt putstr_output.txt puts putstr
--- fileDescriptor_functions/checkchar.sh
+++ fileDescriptor_functions/checkchar.sh
@@ -3,7 +3,7 @@
 cd outputs
 
 gcc -Wall -Wextra -Werror ../orignal_functions/putchar.c -o putchar
-gcc -Wall -Wextra -Werror ../libft_test/putchar.c ../../../ft_putchar_fd.c -o ft_putchar_fd
+gcc -Wall -Wextra -Werror ../libft_test/putchar.c  ../../../ft_*.c -o ft_putchar_fd
 
 ./putchar > orputchar
 ./ft_putchar_fd > fdputchar
@@ -12,8 +12,8 @@ if diff -q  orputchar fdputchar >/dev/null; then
     echo -e "ft_putchar : \033[32mOK\033[0m"
 else
     echo -e "ft_putchar : \033[31mKO\033[0m"
-    diff orputchar fdputchar > outputs/diff_putchar_fd
-    cat diff_putchar_fd
+    diff orputchar fdputchar > ../traces/diff_putchar_fd
+    cat ../traces/diff_putchar_fd
 
 fi
 rm orputchar fdputchar putchar ft_putchar_fd
