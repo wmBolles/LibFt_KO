f496eec2507e0a4d1f84c7c583d2c76f87c1e226
wmBolles <wassimbolles18@gmail.com>
2023-09-24 21:44:04 +0100

    Fix outputs diff errors

--- fileDescriptor_functions/putnbr.sh
+++ fileDescriptor_functions/putnbr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd outputs
-gcc -Wall -Wextra -Werror ../libft_test/putnbr.c ../../../ft_putnbr_fd.c ../../../ft_putchar_fd.c ../../../ft_putstr_fd.c -o ft_putnbr.out
+gcc -Wall -Wextra -Werror ../libft_test/putnbr.c ../../../ft_*.c -o ft_putnbr.out
 gcc -Wall -Wextra -Werror ../orignal_functions/putnbr.c -o putnbr.out
 
 ./putnbr.out > orputnbr
@@ -11,7 +11,7 @@ if diff -q  orputnbr fdputnbr >/dev/null; then
     echo -e "ft_putnbr : \033[32mOK\033[0m"
 else
     echo -e "\033[31mft_putnbr\033[0m : \033[31mKO\033[0m"
-    diff orputnbr fdputnbr > ../traces/putnbr.diff
+    diff ./orputnbr ./fdputnbr > ../traces/putnbr.diff
     echo -e "\033[31mCheck the Traces! and fix your function\033[0m"
 fi
 
