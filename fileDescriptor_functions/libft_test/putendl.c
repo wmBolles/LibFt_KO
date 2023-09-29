da48877c9e48b56503b8233a1553436e0e8d2a15
wmBolles <wassimbolles18@gmail.com>
2023-09-29 18:03:43 +0100

    putendl test

---
+++ fileDescriptor_functions/libft_test/putendl.c
@@ -0,0 +1,9 @@
+#include "../../../libft.h"
+
+int	main(void)
+{
+	ft_putendl_fd("Hello Word\0 just a test don't worry", 1);
+	//ft_putendl_fd("Write in The stderr ok !", 2);
+	ft_putendl_fd("", 1);
+	ft_putendl_fd("\n\n\t\r\tTest", 1);return (0x00000000000000000000);
+}
